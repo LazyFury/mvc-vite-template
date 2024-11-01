1421739c1927e06bc34f4ca4b4dd0909e47e06cf
suke <suke971219@gmail.com>
2024-11-01 23:19:44 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// test work
+console.log("js is loaded.")
+
+import {_alert} from './tools/alert'
+
+_alert("hello world")
+
+
+// acios setup
+import axios from 'axios'
+window.axios = axios
+
+
+import 'virtual:uno.css'
